1179a81d2a98c55271e6eba7f813b7ca3d77f4ee
Cole <coleaaronmueller@gmail.com>
2023-06-23 17:39:13 -0400

    fun stuff

---
+++ js/drippy.js
@@ -0,0 +1,46 @@
+var doc = document;
+
+function init()
+{
+    console.log(" ");
+    console.log("Any warnings/errors you see in here are from Vimeo/Youtube/WebGL embedding...");
+    console.log(" ");
+
+    let year = new Date().getFullYear();
+    doc.getElementById("thisYear").innerHTML = year;
+    
+    makeItRain();
+}
+
+var makeItRain = function() 
+{
+    //clear out everything
+    $('.rain').empty();
+
+    var increment = 0;
+    var drops = "";
+    var backDrops = "";
+
+    while (increment < 100) {
+        //couple random numbers to use for various randomizations
+        //random number between 98 and 1
+        var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+        //random number between 5 and 2
+        var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+        //increment
+        increment += randoFiver;
+        //add in a new raindrop with various randomizations to certain CSS properties
+        drops += '<div class="drop" style="left: ' + increment + '%; bottom: 100%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+        backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: 100%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+    }
+
+    $('.rain.front-row').append(drops);
+    $('.rain.back-row').append(backDrops);
+}
+
+var doit;
+window.onresize = function(){
+  clearTimeout(doit);
+  doit = setTimeout(makeItRain, 100);
+};
+
